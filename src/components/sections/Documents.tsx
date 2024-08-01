695f2d6481883c83c71586e16c823147d0d3b66e
uPirlot <44704823+upirlot@users.noreply.github.com>
2024-08-01 17:55:40 +0200

    Update Documents.tsx

--- src/components/sections/Documents.tsx
+++ src/components/sections/Documents.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from "react";
+import Title from "../ui/Title";
 
 interface DocumentsProps {}
 
 const Documents: FunctionComponent<DocumentsProps> = () => {
-  return <>
-  
-  </>;
+  return (
+    <div className="Documents">
+      <div className="container flex flex-col gap-10">
+        <Title>Doc. Institutions</Title>
+      </div>
+    </div>
+  );
 };
 
 export default Documents;
