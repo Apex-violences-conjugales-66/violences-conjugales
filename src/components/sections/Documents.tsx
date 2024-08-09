6b05ae4ff1bdcbaca5c2f4a84227e438b99855e6
uPirlot <44704823+upirlot@users.noreply.github.com>
2024-08-09 16:55:05 +0200

    wip

--- src/components/sections/Documents.tsx
+++ src/components/sections/Documents.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FunctionComponent } from "react";
 import Title from "../ui/Title";
 
@@ -12,19 +13,38 @@ interface DocumentsProps {
 }
 
 const Documents: FunctionComponent<DocumentsProps> = ({ documents }) => {
+  //get the 9 first elements
+  const first_elements = [];
+  if (!documents) {
+  } else {
+    for (let i = 0; i < (documents.length < 9 ? documents.length : 9); i++) {
+      first_elements.push(documents[i]);
+    }
+  }
+
   return (
-    <div className="Documents">
-      <div className="container flex flex-col gap-10">
+    <div className="Documents my-60">
+      <div className="container flex flex-col gap-20 items-center">
         <Title>Doc. Institutions</Title>
-        {documents.map((document, index) => (
-          <div key={index}>
-            <h2>{document.title}</h2>
-            <img src={document.imagePath} />
-            <a className="underline" href={document.docPath}>
-              Lien vers le document
-            </a>
-          </div>
-        ))}
+        <div className="flex flex-wrap gap-6 justify-center ">
+          {first_elements.map((document, index) => (
+            <div
+              key={`Document${index}`}
+              className="w-72 h-80 relative shadow-sm"
+            >
+              {/* <h2>{document.title}</h2> */}
+              <Image
+                alt={`Image${index}`}
+                src={document.imagePath}
+                style={{ objectFit: "cover" }}
+                fill
+              />
+              {/* <a className="underline" href={document.docPath}>
+                Lien vers le document
+              </a> */}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
