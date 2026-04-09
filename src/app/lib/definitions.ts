28d400a2a93aee7279721a669d21fd75ebdeccb6
uPirlot <44704823+upirlot@users.noreply.github.com>
2026-04-09 19:46:18 +0200

    added db and seeded data

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,68 @@
+// This file contains type definitions for your data.
+// It describes the shape of the data, and what data type each property should accept.
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Partner = {
+  id: string;
+  name: string;
+  type: "associatif" | "institutionnel";
+  imageUrl: string;
+};
+
+export type Member = {
+  id: string;
+  name: string;
+  title: string;
+  type: "bureau" | "administration" | "team";
+};
+
+export type Catalogue = {
+  id: string;
+  year: number;
+  url: string;
+  uploadedAt: Date;
+};
+
+export type Formation = {
+  id: string;
+  name: string;
+  date: string;
+  year: number;
+  status: "available" | "full";
+  target: string;
+  instructor: string;
+};
+
+export type DocumentResource = {
+  id: string;
+  name: string;
+  documentUrl: string;
+};
+
+export type Memoir = {
+  id: string;
+  author: string;
+  title: string;
+  description: string;
+  documentUrl: string;
+};
+
+export type Book = {
+  id: string;
+  author: string;
+  title: string;
+  description: string;
+};
+
+export type Movie = {
+  id: string;
+  title: string;
+  description: string;
+  movieUrl: string;
+};
